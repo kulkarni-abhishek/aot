6c11f631c699cc9c15f7ce7f88736f67a7f95232
Abhishek.kulkarni <abhishek.kulkarni@springct.com>
2022-12-16 17:24:00 +0530

    Initial commit

---
+++ src/test/java/com/example/aottest/AotTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AotTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
